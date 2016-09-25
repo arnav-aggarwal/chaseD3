76b64b0ec99145f7b703a7cd039c0721a72a3b89
Arnav Aggarwal <arnavaggrwl@gmail.com>
2016-09-25 15:03:12 -0700

    (feat) Circle of dots moves with mouse
    256 dots are rendered to the page and follow the mouse around
    in a radius of 75. Accomplished by setting global variables mouseX
    and mouseY and changing them on mousemove. In addition, the dots
    are re-rendered to a random position of distance 75 from the mouse
    ever 15 ms. The result is a ring of dots constantly moving with the
    mouse.

--- chaseme.js
+++ chaseme.js
@@ -1,47 +1,58 @@
 const container = document.getElementById('container');
-
 const width = $(document).width();
 const height = $(document).height();
 
-const abs = Math.abs;
-
-const svg = d3.select("#container").append("svg")
-  .attr("width", width)
-  .attr("height", height)
-  // .attr("class", "overlay");
-
-const dots = [
+const dots4 = [
   {x: width / 4, y: height / 4}, 
   {x: width * 3/4, y: height * 3/4},
   {x: width / 4, y: height * 3/4},
   {x: width * 3/4, y: height / 4},
 ];
+const dots16 = [...dots4, ...dots4, ...dots4, ...dots4];
+const dots64 = [...dots16, ...dots16, ...dots16, ...dots16];
+const dots256 = [...dots64, ...dots64, ...dots64, ...dots64];
 
+//Global variables, for dirty-checking mouse position
+let mouseX, mouseY;
+let previousX, previousY;
+
+const svg = d3.select("#container").append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+//Place 256 dots on the page; will look like 4 dots,
+//but each dot is stacked 64 deep
 svg.append("g")
   .attr("class", "dot")
   .selectAll("circle")
-  .data(dots)
+  .data(dots256)
   .enter().append("circle")
   .attr("r", 10)
   .attr("cx", d => d.x)
   .attr("cy", d => d.y);
 
-const circles = d3.selectAll('circle');
-
+//Took the strategy from
 //http://bl.ocks.org/adamhurst/13dd439047b66ee78c45
+//Transition the circles to the mouse's x-position
 svg.on('mousemove', function() {
-  const [x] = d3.mouse(this);
-  circles.transition(1000)
-    .attr('cx', x);
+  [mouseX, mouseY] = d3.mouse(this);
 });
 
-function generateDots() {
-  const circles = d3.selectAll('circle');
+function moveDotsToMouse() {
+  if(previousX !== mouseX) {
+    d3.selectAll('circle')
+      .each(function(d) {
+        const radius = 75;
+        const degree = Math.random() * 360;
+        const circleX = Math.cos(degree) * radius;
+        const circleY = Math.sin(degree) * radius;
+        d3.select(this)
+          .attr('cx', mouseX + circleX)
+          .attr('cy', mouseY + circleY);
+      });
+  }
 
+  [previousX, previousY] = [mouseX, mouseY];
 }
 
-function draw() {
-  const dots = generateDots();
-}
-
-setInterval(draw, 20);
+setInterval(moveDotsToMouse, 15);
