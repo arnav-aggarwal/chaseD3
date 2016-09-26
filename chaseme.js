31cc724a971c8ceaa080754e609b67ea39f353ef
Arnav Aggarwal <arnavaggrwl@gmail.com>
2016-09-26 00:50:57 -0700

    (feat) Circle grows and accelerates upon dot addtion
    -Debugging of empty spot in circle
    
    -Cleanup and commenting of code
    
    -Removal of unnecessary css

--- chaseme.js
+++ chaseme.js
@@ -1,28 +1,33 @@
 const pageWidth = window.innerWidth;
 const pageHeight = window.innerHeight;
-const TOTAL_DOTS = 16;
-const rotatingDots = [];
+const TOTAL_DOTS = 12;
 const dots = [];
 
 //Global variables
+//mouseX and mouseY are initialized high to ensure that no
+//dots start rotating without a mousemove event
 let mouseX = pageWidth * 2;
 let mouseY = pageHeight * 2;
 let dotNum = 0;
-let numRotating = 0;
+let numRevolving = 0;
 
 //Fill up dots array
 while(dotNum++ < TOTAL_DOTS) {
+  const margin = 200;
   dots.push({
-    x: Math.random() * pageWidth,
-    y: Math.random() * pageHeight,
+    x: Math.random() * (pageWidth - margin) + margin / 2,
+    y: Math.random() * (pageHeight - margin) + margin / 2,
   });
 }
 
-//Append dots to the page
 const svg = d3.select('#container').append('svg')
   .attr('width', pageWidth)
-  .attr('height', pageHeight);
+  .attr('height', pageHeight)
+  .on('mousemove', function() {
+    [mouseX, mouseY] = d3.mouse(this);
+  });
 
+//Append dots to the page
 svg.append('g')
   .attr('class', 'dot')
   .selectAll('circle')
@@ -31,47 +36,53 @@ svg.append('g')
   .attr('r', 10)
   .attr('cx', dot => dot.x)
   .attr('cy', dot => dot.y)
-  .classed('notRotating', true);
+  .classed('notRevolving', true);
 
-//Took this strategy from
+//Took the general strategy of combining a mousemove
+//event listener with a setInterval from
 //http://bl.ocks.org/adamhurst/13dd439047b66ee78c45
-//Transition the circles to the mouse's x-position
-svg.on('mousemove', function() {
-  [mouseX, mouseY] = d3.mouse(this);
-});
 
-function circleDotsAroundMouse(offset = 0) {
+/**
+ * Find every dot with a class 'revolving' and revolve it repeatedly
+ * @param  {Number} offset - this is what actually does the revolving
+ */
+function revolveDots(offset = 0) {
   collectDots();
-  d3.selectAll('.rotating')
+  d3.selectAll('.revolving')
     .each(function(dot) {
-      const radius = 75;
-      const degree = 2 * Math.PI * dot.number / numRotating + offset;
+      const radius = 5 * numRevolving;
+      const degree = 2 * Math.PI * dot.number / numRevolving + offset;
       const circleX = Math.cos(degree) * radius;
       const circleY = Math.sin(degree) * radius;
       d3.select(this)
         .transition()
-        .duration(50)
+        .duration(25)
         .attr('cx', mouseX + circleX)
         .attr('cy', mouseY + circleY);
     });
 
-  setTimeout(circleDotsAroundMouse.bind(null, offset + 0.025), 50);
+  //the bound argument here is what does the revolving
+  setTimeout(revolveDots.bind(null, offset + 0.003 * numRevolving), 25);
 } 
 
+/**
+ * Gather every non-revolving dot and check to see if it's within range;
+ * if so, change the notRevolving class to revolving
+ */
 function collectDots() {
   const radius = 75;
-  d3.selectAll('.notRotating')
+  d3.selectAll('.notRevolving')
     .each(function(dot) {
       const xDistance = dot.x - mouseX;
       const yDistance = dot.y - mouseY;
       const distance = Math.sqrt(xDistance ** 2 + yDistance ** 2);
       if(distance < radius) {
-        dot.number = numRotating++;
+        dot.number = numRevolving++;
         d3.select(this)
-          .classed('rotating', true)
-          .classed('notRotating', false);
+          .classed('revolving', true)
+          .classed('notRevolving', false);
       }
     });
 }
 
-circleDotsAroundMouse();
+revolveDots();
