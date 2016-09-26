f2efcbce45a28badd94b3a58ec8d8e07a05918ab
Arnav Aggarwal <arnavaggrwl@gmail.com>
2016-09-25 23:25:39 -0700

    (feat) Make dots align when close enough

--- chaseme.js
+++ chaseme.js
@@ -1,21 +1,22 @@
 const pageWidth = window.innerWidth;
 const pageHeight = window.innerHeight;
-const NUM_DOTS = 16;
-
+const TOTAL_DOTS = 16;
+const rotatingDots = [];
 const dots = [];
+
+//Global variables
+let mouseX = pageWidth * 2;
+let mouseY = pageHeight * 2;
 let dotNum = 0;
-while(dotNum < NUM_DOTS) {
+let numRotating = 0;
+
+//Fill up dots array
+while(dotNum++ < TOTAL_DOTS) {
   dots.push({
-    number: dotNum++,
     x: Math.random() * pageWidth,
     y: Math.random() * pageHeight,
   });
 }
-
-//Global variables, for dirty-checking mouse position
-let mouseX = pageWidth / 2;
-let mouseY = pageHeight / 2;
-let previousX, previousY;
 
 //Append dots to the page
 const svg = d3.select('#container').append('svg')
@@ -29,7 +30,8 @@ svg.append('g')
   .enter().append('circle')
   .attr('r', 10)
   .attr('cx', dot => dot.x)
-  .attr('cy', dot => dot.y);
+  .attr('cy', dot => dot.y)
+  .classed('notRotating', true);
 
 //Took this strategy from
 //http://bl.ocks.org/adamhurst/13dd439047b66ee78c45
@@ -38,59 +40,38 @@ svg.on('mousemove', function() {
   [mouseX, mouseY] = d3.mouse(this);
 });
 
-const updateMousePosition = () => [previousX, previousY] = [mouseX, mouseY];
-
 function circleDotsAroundMouse(offset = 0) {
-  d3.selectAll('circle')
+  collectDots();
+  d3.selectAll('.rotating')
     .each(function(dot) {
       const radius = 75;
-      const degree = 2 * Math.PI * dot.number / NUM_DOTS  + offset;
+      const degree = 2 * Math.PI * dot.number / numRotating + offset;
       const circleX = Math.cos(degree) * radius;
       const circleY = Math.sin(degree) * radius;
       d3.select(this)
+        .transition()
+        .duration(50)
         .attr('cx', mouseX + circleX)
         .attr('cy', mouseY + circleY);
     });
 
-  updateMousePosition();
-  setTimeout(circleDotsAroundMouse.bind(null, offset + 0.012),  20);
-}
+  setTimeout(circleDotsAroundMouse.bind(null, offset + 0.025), 50);
+} 
 
-//currently collects dots
 function collectDots() {
-  const radius = 100;
-  d3.selectAll('circle')
+  const radius = 75;
+  d3.selectAll('.notRotating')
     .each(function(dot) {
-      const xDiff = dot.x - mouseX;
-      const yDiff = dot.y - mouseY;
-
-      let quadrant;
-      if(xDiff > 0) {
-        quadrant = yDiff > 0 ? 1 : 4;
-      } else {
-        quadrant = yDiff > 0 ? 2 : 3;
-      }
-
-      Math.sqrt(xDiff ** 2 + yDiff ** 2) < 25 && console.log('yay!')
-      if(Math.sqrt(xDiff ** 2 + yDiff ** 2) < radius) {
-        const angle = Math.atan(yDiff / xDiff);
+      const xDistance = dot.x - mouseX;
+      const yDistance = dot.y - mouseY;
+      const distance = Math.sqrt(xDistance ** 2 + yDistance ** 2);
+      if(distance < radius) {
+        dot.number = numRotating++;
         d3.select(this)
-          .transition(9)
-          .attr('cx', function() {
-
-            dot.x += Math.cos(Math.abs(angle)) * (radius * (xDiff > 0 ? 1: -1)  - xDiff);
-            return dot.x;
-          })
-          .attr('cy', function() {
-            dot.y += Math.sin(Math.abs(angle)) * (radius * (yDiff > 0 ? 1: -1) - yDiff);
-            return dot.y;
-          });
+          .classed('rotating', true)
+          .classed('notRotating', false);
       }
     });
-
-  updateMousePosition();
-  setTimeout(collectDots, 20);
 }
 
 circleDotsAroundMouse();
-// setInterval(moveDotsAway, 10);
